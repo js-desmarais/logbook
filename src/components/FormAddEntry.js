b0e5455ff1d5b0da2111b5e628554913b10ac4c7
js-desmarais <js.desmarais@yahoo.ca>
2023-09-12 23:56:23 -0400

    quick fix to make it work

--- src/components/FormAddEntry.js
+++ src/components/FormAddEntry.js
@@ -3,7 +3,8 @@ import { EntryField } from "./EntryField";
 import { Button } from "./Button";
 
 export function FormAddEntry({ onAddEntry }) {
-	const [entry, setEntry] = useState({});
+	const [entry, setEntry] = useState({ type: "test", registration: "CGORE" });
+	// const { [date, setDate], [type, setType], [registration, setRegistration], [origin, setOrigin], [destination, setDestination], [pilot, setPilot], [copilot, setCopilot], [duration, setDuration], [takeoffs, setTakeoffs], [landings, setLandings] } = useState();
 	const { date, type, registration, origin, destination, pilot, copilot, duration, takeoffs, landings } = entry;
 
 	function handleSubmit(e) {
@@ -12,10 +13,9 @@ export function FormAddEntry({ onAddEntry }) {
 		if (entry === {}) return;
 
 		const id = crypto.randomUUID();
-
 		const newEntry = {
 			id,
-			date,
+			date: new Date(date),
 			type,
 			registration,
 			origin,
@@ -27,39 +27,51 @@ export function FormAddEntry({ onAddEntry }) {
 			landings,
 		};
 
-		// console.log(newEntry, entry);
-		// onAddEntry();
+		console.log(newEntry);
+		onAddEntry(newEntry);
 		// setEntry({});
 	}
+
+	// BUG these don't work. Either passes p as a key name, or overwrites the entire object.
+	/* 	function handleOnSet(p) {
+		return e => {
+			entry[p] = e.target.value;
+			setEntry({ ...entry });
+		};
+		// return e => setEntry((entry[p] = e.target.value));
+		// setEntry({
+		// 	...entry,
+		// 	entry[p]= e.target.value,
+		// });
+	} */
 
 	return (
 		<div className="addEntry">
 			<h3>Add flight details...</h3>
 			<form className="addEntry__form" onSubmit={handleSubmit}>
-				<EntryField type="date" name="date" onSet={e => setEntry({ date: e.target.value })} />
 				{/* <EntryField type="date" name="date" onSet={handleOnSet(date)} /> */}
 
-				<EntryField type="text" name="type" onSet={e => setEntry({ type: e.target.value })} />
+				<EntryField type="date" name="date" onSet={e => setEntry({ ...entry, date: e.target.value })} />
 
-				<EntryField type="text" name="registration" onSet={e => setEntry({ registration: e.target.value })} />
+				<EntryField type="text" name="type" onSet={e => setEntry({ ...entry, type: e.target.value })} />
 
-				<EntryField type="text" name="origin" onSet={e => setEntry({ origin: e.target.value })} />
+				<EntryField type="text" name="registration" onSet={e => setEntry({ ...entry, registration: e.target.value })} />
 
-				<EntryField type="text" name="destination" onSet={e => setEntry({ destination: e.target.value })} />
+				<EntryField type="text" name="origin" onSet={e => setEntry({ ...entry, origin: e.target.value })} />
 
-				<EntryField type="text" name="pilot" onSet={e => setEntry({ pilot: e.target.value })} />
+				<EntryField type="text" name="destination" onSet={e => setEntry({ ...entry, destination: e.target.value })} />
 
-				<EntryField type="text" name="copilot" onSet={e => setEntry({ copilot: e.target.value })} />
+				<EntryField type="text" name="pilot" onSet={e => setEntry({ ...entry, pilot: e.target.value })} />
 
-				<EntryField type="number" name="duration" onSet={e => setEntry({ duration: e.target.value })} />
+				<EntryField type="text" name="copilot" onSet={e => setEntry({ ...entry, copilot: e.target.value })} />
 
-				<EntryField type="number" name="takeoffs" onSet={e => setEntry({ takeoffs: e.target.value })} />
+				<EntryField type="number" name="duration" onSet={e => setEntry({ ...entry, duration: e.target.value })} />
 
-				<EntryField type="number" name="landings" onSet={e => setEntry({ landings: e.target.value })} />
+				<EntryField type="number" name="takeoffs" onSet={e => setEntry({ ...entry, takeoffs: e.target.value })} />
 
-				<Button className="btn--active" onClick={handleSubmit}>
-					Submit
-				</Button>
+				<EntryField type="number" name="landings" onSet={e => setEntry({ ...entry, landings: e.target.value })} />
+
+				<Button className="btn--active">Submit</Button>
 			</form>
 		</div>
 		// <input type="text" onChange={handleOnSet()}></input>
